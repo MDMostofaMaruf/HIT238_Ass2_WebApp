f311dd84bc82fe215c756304a1edfffc97c9646c
MDMostofaMaruff <kazi.maruf123@gmail.com>
2020-10-10 23:36:57 +0930

    Service Worker Added

---
+++ js/main.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator){
+console.log("service worker supported")
+window.addEventListener('load',()=>{
+    navigator.serviceWorker
+    .register('../ServiceWorker.js')
+    .then(reg => console.log('service worker:regsiter'))
+    .catch(err => console.log("service worker:error"))
+    
+})
+
+}
