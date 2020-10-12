da85f2cb16366b3c7b9aa16d35aaa330e4d7d642
MDMostofaMaruff <kazi.maruf123@gmail.com>
2020-10-13 01:35:28 +0930

    Design changed for the appinmnet form

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator){
     console.log("service worker supported")
     window.addEventListener('load',()=>{
         navigator.serviceWorker
-        .register('/HIT238_Ass2_WebApp/ServiceWorker.js')
+        .register('HIT238_Ass2_WebApp/js/ServiceWorker.js')
         .then(Reg => console.log('service worker:regsiter', Reg))
         .catch(err => console.log(`error: ${err}`))
         
