6e04c03a3c1f98bdf39434edbead7e16657f33b0
MDMostofaMaruff <kazi.maruf123@gmail.com>
2020-10-12 23:45:01 +0930

    working on style

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator){
     console.log("service worker supported")
     window.addEventListener('load',()=>{
         navigator.serviceWorker
-        .register('js/ServiceWorker.js')
+        .register('/ServiceWorker.js')
         .then(Reg => console.log('service worker:regsiter'))
         .catch(err => console.log(`error: ${err}`))
         
