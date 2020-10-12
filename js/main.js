ead5685893ac6b0f20fc0cdab384d9a8c6f2bd26
MDMostofaMaruff <kazi.maruf123@gmail.com>
2020-10-13 01:01:04 +0930

    service worker scope

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator){
     console.log("service worker supported")
     window.addEventListener('load',()=>{
         navigator.serviceWorker
-        .register('/ServiceWorker.js')
+        .register('/HIT238_Ass2_WebApp/ServiceWorker.js')
         .then(Reg => console.log('service worker:regsiter', Reg))
         .catch(err => console.log(`error: ${err}`))
         
